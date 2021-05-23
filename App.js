30576475457be6e0d1ffac84bfae83ec9316b909
codefmn <msgfmn@163.com>
2021-05-23 19:41:37 +0800

    Add elements

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  Header,
+  LinearProgress,
+  Slider,
+  Icon,
+  Button,
+} from 'react-native-elements';
 
 export default function App() {
+  const [age, setAge] = useState(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header
+        leftComponent={{ icon: 'menu', color: '#fff' }}
+        centerComponent={{ text: 'MOBILE APP', style: { color: '#fff' } }}
+        rightComponent={{ icon: 'home', color: '#fff' }}
+      />
+      <LinearProgress color='primary' value={0} />
+      <Text>How old are you ?</Text>
+      <View
+        style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center' }}
+      >
+        <Slider
+          value={10}
+          onValueChange={(val) => setAge(val)}
+          maximumValue={150}
+          minimumValue={0}
+          step={1}
+          trackStyle={{ height: 5, backgroundColor: 'transparent' }}
+          thumbStyle={{ height: 10, width: 10, backgroundColor: 'transparent' }}
+          thumbProps={{
+            children: (
+              <Icon
+                name='eercast'
+                type='font-awesome'
+                size={10}
+                reverse
+                containerStyle={{ bottom: 10, right: 10 }}
+                color='#0af'
+              />
+            ),
+          }}
+        />
+        <Text>Age: {age}</Text>
+      </View>
+
+      <Button title='Continue' />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -15,7 +57,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
   },
 });
